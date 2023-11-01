4093c9a976ec2ae2f8eea06be441efb62c88f5a2
Carlos Espinosa <carloespinosa88@gmail.com>
2023-10-31 23:29:44 -0500

    add react native paper

--- src/screens/SlideOne.js
+++ src/screens/SlideOne.js
@@ -1,47 +1,54 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useTheme } from 'react-native-paper';
 import { ImageOne } from '../images'
 import Button from '../components/Button'
 
 
 function SlideOne({ navigation }) {
+  const theme = useTheme();
+
   return (
-    <View style={styles.mainContainer}>
-      <View style={styles.imageContainer}>
-        <View style={styles.imageContainerText}>
-          <Image source={ImageOne} style={styles.image} />
-          <View style={styles.labelContainer}>
-            <Text style={styles.mainText}>Tos</Text>
-          </View> 
-        </View>  
+    <SafeAreaProvider>
+      <View style={styles.mainContainer}>
+        <View style={styles.imageContainer}>
+          <View style={styles.imageContainerText}>
+            <Image source={ImageOne} style={styles.image} />
+            <View style={styles.labelContainer}>
+              <Text style={styles.mainText}>Tos</Text>
+            </View> 
+          </View>  
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title='NO'
+            style={styles.buttonTwo}
+            color={styles.colorButtonTwo}
+            onPress={() => {
+              navigation.navigate('SlideTwo', {
+                values: { atormentado: false }
+              })
+            }}
+          />
+          <Button
+            title='SI'
+            style={styles.buttonOne}
+            onPress={() => {
+              navigation.navigate('SlideTwo', {
+                values: { atormentado: true }
+              })
+            }} 
+          />
+        </View>
       </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          title='NO'
-          style={styles.buttonTwo}
-          color={styles.colorButtonTwo}
-          onPress={() => {
-            navigation.navigate('SlideTwo', {
-              values: { atormentado: false }
-            })
-          }}
-        />
-        <Button
-          title='SI'
-          style={styles.buttonOne}
-          onPress={() => {
-            navigation.navigate('SlideTwo', {
-              values: { atormentado: true }
-            })
-          }} 
-        />
-      </View>
-    </View>
+    </SafeAreaProvider>
   );
 }
+// theme.colors.primary
 
 const styles = StyleSheet.create({
-  mainContainer: { 
+  mainContainer: {
     flex: 1,
     marginTop: 90,
     paddingHorizontal: 20
