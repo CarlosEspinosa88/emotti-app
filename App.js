63eb19f73d15c96e88595613f14e5c5619789bbf
Carlos Espinosa <carloespinosa88@gmail.com>
2023-10-31 23:29:44 -0500

    add safe area view provider

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import {
+  SafeAreaProvider,
+  SafeAreaView
+} from 'react-native-safe-area-context';
 import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
 import MainStack from './src/navigators'
 
@@ -10,15 +14,17 @@ const theme = {
     secondary: 'yellow',
     white: 'white',
     purple: '#663399',
-    ripple: 'rgba(255, 255, 255, 0.5)'
+    ripple: 'rgba(255, 255, 255, 0.5)',
+    darkRipple: 'rgba(0, 0, 0, 0.5)'
   },
 };
 
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
-      <MainStack />
-    </PaperProvider>
-
+    <SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <MainStack />
+      </PaperProvider>
+    </SafeAreaProvider>
   ) 
 }
